b669005581823ca62cfc776f6b26ac288922a8bc
Ahmed194724496 <az7923140@gmail.com>
2025-10-25 23:40:45 +0300

    Last

--- shop/urls.py
+++ shop/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include  
-from django.http import JsonResponse
+from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 def home(request):
-    return JsonResponse({'message': 'Shop API is working!'})
+
+    return HttpResponseRedirect('/products/products/')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('products/', include('products.urls')),  
-    path('', home),
+    path('products/', include('products.urls')),
+    path('', home),  
 ]
